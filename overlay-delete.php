13d731b29315cd2462539ef74c29096b7fda35ac
Owenby, Craig Michael <craig.owenby@gatech.edu>
2016-03-20 15:40:57 -0400

    Added server deletion of image created using PHP

--- overlay-delete.php
+++ overlay-delete.php
@@ -2,6 +2,6 @@
 
 define('DIR_LOC', 'images/');
 $img = filter_input($_POST['img'], FILTER_SANITIZE_STRING);
-$file_path = DIR_LOC . img . '.png';
+$file_path = DIR_LOC . $img . '.png';
 $successful_delete = unlink($file_path);
 print $successful_delete ? 'Successfully deleted.' : 'Deletion unsuccessful.';
