b6f27d1c7798171721db2b4ce703ef507a61b53d
Owenby, Craig Michael <craig.owenby@gatech.edu>
2016-03-20 00:47:42 -0400

    Uploads overlay to php server

--- php/overlay-upload.php
+++ php/overlay-upload.php
@@ -5,8 +5,7 @@
     // requires php5
 	define('UPLOAD_DIR', 'images/');
 	$img = $_POST['img'];
-
-    print $data;
+    echo '<img src="data:image/gif;base64,' . $img . '" />';
 	$img = str_replace('data:image/png;base64,', '', $img);
 	$img = str_replace(' ', '+', $img);
 	$data = base64_decode($img);
