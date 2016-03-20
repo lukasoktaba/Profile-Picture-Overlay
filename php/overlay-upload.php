39cdf6dbd0d85196a7806be612186f10870f7297
Owenby, Craig Michael <craig.owenby@gatech.edu>
2016-03-20 00:11:32 -0400

    Uploads overlay to php server

--- php/overlay-upload.php
+++ php/overlay-upload.php
@@ -8,6 +8,7 @@
 	$img = str_replace('data:image/png;base64,', '', $img);
 	$img = str_replace(' ', '+', $img);
 	$data = base64_decode($img);
+    print $data;
 	$file = UPLOAD_DIR . uniqid() . '.png';
 	$success = file_put_contents($file, $data);
 	print $success ? $file : 'Unable to save the file.';
